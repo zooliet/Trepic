3492f050c486872a8eac5b94203e23b928982456
Junhyun Shin <hl1sqi@gmail.com>
2016-01-29 17:08:17 +0900

    Destroy a tweet

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -8,5 +8,9 @@ class TweetsController < ApplicationController
   end
 
   def destroy
+    @tweet = Tweet.find(params[:id])
+    @tweet.destroy
+
+    redirect_to tweets_path, notice: "Tweet was successfully destroyed"
   end
 end
